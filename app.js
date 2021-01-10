7744b3ee228f42ffe3876ecb37120c953c7f26fe
ihab1990 <ihab.abumulaweh@gmail.com>
2021-01-10 15:15:44 +0200

    change

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var seatle ={
+minCustomer = 23,
+maxCustomer = 65,
+avgCookies = 6.3,
+
+calculateCustomersPerHour : function () {
+}
+}
+
+var tokyo ={
+    minCustomer = 3,
+    maxCustomer = 24,
+    avgCookies = 1.2,
+    
+    calculateCustomersPerHour : function () {
+    }
+    }
+
+    var dubai ={
+        minCustomer = 11,
+        maxCustomer = 38,
+        avgCookies = 3.7,
+        
+        calculateCustomersPerHour : function () {
+        }
+        }
+
+        var paris ={
+            minCustomer = 20,
+            maxCustomer = 38,
+            avgCookies = 2.3,
+            
+            calculateCustomersPerHour : function () {
+            }
+            }
+
+            var lima ={
+                minCustomer = 2,
+                maxCustomer = 16,
+                avgCookies = 4.6,
+                
+                calculateCustomersPerHour : function () {
+                }
+                }
